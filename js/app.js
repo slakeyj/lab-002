c3dfed21179d9dd20222683bff238de848b64069
Sharina Stubbs <sharinastubbs@gmail.com>
2019-07-11 16:32:57 -0700

    Created arrays for yes responses and no responses.

--- js/app.js
+++ js/app.js
@@ -6,6 +6,14 @@ var userName;
 //Variable to track correct answers.
 var totalCorrect = 0;
 
+
+var correctResponses = ['y', 'yes', 'n', 'no'];
+
+var yesAlerts = ['That\'s incorrect, but she hopes to someday!', 'That\'s correct! It helps keep her feel low-stress and bendy!', 'That is incorrect.  She finds them to be absolutely repulsive!', 'That\'s incorrect.  She grew up in Eastern Washington, but has lived in Seattle for 9 years.', 'That\'s correct! She has 2 brothers and 1 sister.'];
+
+var noAlerts = ['That\'s correct!  Hopefully she\'ll get to go someday!', 'That\'s incorrect!  Susanna enjoys yoga to keep herself flexible and to decrease her stress levels.', 'That\'s correct!  She finds them to be absolutely repulsive!', 'That\'s correct! She grew up in Eastern Washington, but has lived in Seattle for 9 years.', 'That\'s incorrect! She has 2 brothers and 1 sister.'];
+
+
 //Asks for the user's name.
 function askName() {
   userName = prompt('What is your name?');
@@ -13,7 +21,7 @@ function askName() {
   console.log('The user\'s name is ' + userName + '.');
 }
 
-askName ();
+
 
 
 
@@ -31,7 +39,7 @@ function askJapan() {
   console.log('The answer given to beenToJapan is ' + beenToJapan);
 }
 
-askJapan();
+
 
 // Prompt asking if Susanna enjoys doing yoga.
 function questionYoga() {
@@ -47,7 +55,7 @@ function questionYoga() {
   console.log('The answer given to enjoysYoga is ' + enjoysYoga);
 }
 
-questionYoga();
+
 
 //Prompt asking if Susanna's favorite vegetable is radishes.
 function questionRadish() {
@@ -61,8 +69,8 @@ function questionRadish() {
     console.log('The total correct is ' + totalCorrect);
   }
   console.log('The answer given to likesRadishes is ' + likesRadishes);
-  }
-questionRadish();
+}
+
 
 //Prompts asking if Susanna has lived in Seattle all of her life.
 function questionSeattle() {
@@ -76,8 +84,8 @@ function questionSeattle() {
     console.log('The total correct is ' + totalCorrect);
   }
   console.log('The answer given to fromSeattle is ' + fromSeattle);
-  }
-questionSeattle();
+}
+
 
 //Prompts asking if Susanna has 3 siblings.
 function questionSibling(){
@@ -92,59 +100,73 @@ function questionSibling(){
   }
   console.log('The answer given to siblings is ' + siblings);
 }
-questionSibling();
 
+/*
 //A guessing game to guess how many years I've been married.
-
-for (var i = 0; i < 4; i++) {
-  var yearsMarried = prompt('Question 6: how many years has Susanna been married?');
-  console.log('The user entered the number ' + yearsMarried + ' for yearsMarried.');
-  if (yearsMarried > 8) {
-    alert('Sorry, too high!');
-  } else if (yearsMarried < 8) {
-    alert('Sorry, too low!');
-  } else {
-    alert('That\'s correct!');
-    totalCorrect++;
-    console.log('The total correct is ' + totalCorrect);
-    break;
-  }
-  if (i === 3) {
-    alert('Sorry, you\'re out of guesses. I\'ve been married for 8 years.');
-  }
-}
-
-//A game to guess a Seattle neighborhood I have lived in.
-
-var seattleNeighborhoods = ['lower queen anne', 'capitol hill', 'ballard', 'magnolia'];
-var correct = false;
-var guessNumber = 0;
-
-while (guessNumber < 6 && correct === false) {
-  var neighborhoodGuess = prompt('Question 7: Can you guess a Seattle neighborhood that Susanna has lived in?');
-  neighborhoodGuess = neighborhoodGuess.toLowerCase();
-
-  for (var neighborhoodIndex = 0; neighborhoodIndex < seattleNeighborhoods.length; neighborhoodIndex++) {
-    var neighborhood = seattleNeighborhoods[neighborhoodIndex];
-    if (neighborhood === neighborhoodGuess) {
-      alert('Well done, that\'s correct!  I\'ve lived in Lower Queen Anne, Ballard, Capitol Hill, and Magnolia.');
-      console.log('The user guessed ' + neighborhoodGuess + ' for neighborhoodGuess.');
-      correct = true;
+function questionMarried() {
+  for (var i = 0; i < 4; i++) {
+    var yearsMarried = prompt('Question 6: how many years has Susanna been married?');
+    console.log('The user entered the number ' + yearsMarried + ' for yearsMarried.');
+    if (yearsMarried > 8) {
+      alert('Sorry, too high!');
+    } else if (yearsMarried < 8) {
+      alert('Sorry, too low!');
+    } else {
+      alert('That\'s correct!');
       totalCorrect++;
       console.log('The total correct is ' + totalCorrect);
+      break;
+    }
+    if (i === 3) {
+      alert('Sorry, you\'re out of guesses. I\'ve been married for 8 years.');
     }
   }
-  if (correct === false && guessNumber < 5) {
-    console.log('That is incorrect');
-    alert('Sorry, that\'s incorrect.  Please try again.');
-  } else if (correct === false && guessNumber < 6) {
-    alert('Sorry, you\'re all out of guesses. I\'ve lived in Lower Queen Anne, Ballard, Capitol Hill, and Magnolia.');
-  }
-  guessNumber++;
 }
+
+
+//A game to guess a Seattle neighborhood I have lived in.
+function questionNeighborhoods() {
+  var seattleNeighborhoods = ['lower queen anne', 'capitol hill', 'ballard', 'magnolia'];
+  var correct = false;
+  var guessNumber = 0;
+
+  while (guessNumber < 6 && correct === false) {
+    var neighborhoodGuess = prompt('Question 7: Can you guess a Seattle neighborhood that Susanna has lived in?');
+    neighborhoodGuess = neighborhoodGuess.toLowerCase();
+
+    for (var neighborhoodIndex = 0; neighborhoodIndex < seattleNeighborhoods.length; neighborhoodIndex++) {
+      var neighborhood = seattleNeighborhoods[neighborhoodIndex];
+      if (neighborhood === neighborhoodGuess) {
+        alert('Well done, that\'s correct!  I\'ve lived in Lower Queen Anne, Ballard, Capitol Hill, and Magnolia.');
+        console.log('The user guessed ' + neighborhoodGuess + ' for neighborhoodGuess.');
+        correct = true;
+        totalCorrect++;
+        console.log('The total correct is ' + totalCorrect);
+      }
+    }
+    if (correct === false && guessNumber < 5) {
+      console.log('That is incorrect');
+      alert('Sorry, that\'s incorrect.  Please try again.');
+    } else if (correct === false && guessNumber < 6) {
+      alert('Sorry, you\'re all out of guesses. I\'ve lived in Lower Queen Anne, Ballard, Capitol Hill, and Magnolia.');
+    }
+    guessNumber++;
+  }
+}
+
+
+askName();
+askJapan();
+questionYoga();
+questionRadish();
+questionSeattle();
+questionSibling();
+questionMarried();
+questionNeighborhoods();
 
 
 
 //Gives the user their final score.
 alert('Thank you for playing, ' + userName + '! Your final score is ' + totalCorrect + ' out of 7.');
 
+*/
