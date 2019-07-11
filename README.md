17516bea28b74a71534f87d23ac707c3eedacb61
Susanna Lakey <susannalakey@gmail.com>
2019-07-10 19:55:56 -0700

    Added a new header image and updated some other css styles

--- README.md
+++ README.md
@@ -4,3 +4,7 @@
 
 ### Accessibility Score: 
 https://drive.google.com/file/d/1uQNVuWt3rkSx1yFDC5isYhcJaQx2baLK/view?usp=sharing
+
+###What this project is:
+
+This website uses javascript to prompt the user to ask questions.  It then gives the user a score at the end.  The main page has been styled with CSS and allows the user to know who I am.
--- js/app.js
+++ js/app.js
@@ -1,10 +1,13 @@
-'use strict';
+/*'use strict';
 
 //Asks for the user's name.
-/*
+
 var userName = prompt('What is your name?');
 alert('Hello ' + userName + '!');
-console.log('The user\'s name is ' + userName + '.')
+console.log('The user\'s name is ' + userName + '.');
+
+//Variable to track correct answers.
+var totalCorrect = 0;
 //Prompt asking if Susanna has ever been to Japan.
 
 var beenToJapan = prompt('Question 1: Has Susanna ever been to Japan?');
@@ -13,6 +16,7 @@ if (beenToJapan.toLowerCase() === 'yes' || beenToJapan.toLowerCase() === 'y') {
   alert('That\'s incorrect, but she hopes to someday!');
 } else {
   alert('That\'s correct!  Hopefully she\'ll get to go someday!');
+  totalCorrect++;
 }
 console.log('The answer given to beenToJapan is ' + beenToJapan);
 
@@ -21,6 +25,7 @@ var enjoysYoga = prompt('Question 2: Does Susanna enjoy doing yoga?');
 
 if (enjoysYoga.toLowerCase() === 'yes' || enjoysYoga.toLowerCase() === 'y') {
   alert('That\'s correct! It helps keep her feel low-stress and bendy!');
+  totalCorrect++;
 } else {
   alert('That\'s incorrect!  Susanna enjoys yoga to keep herself flexible and to decrease her stress levels.');
 }
@@ -33,31 +38,34 @@ if (likesRadishes.toLowerCase() === 'yes' || likesRadishes.toLowerCase() === 'y'
   alert('That is incorrect.  She finds them to be absolutely repulsive!');
 } else {
   alert('That\'s correct!  She finds them to be absolutely repulsive!');
+  totalCorrect++;
 }
 console.log('The answer given to likesRadishes is ' + likesRadishes);
 
 //Prompts asking if Susanna has lived in Seattle all of her life.
-var fromSeattle = prompt('Has Susanna lived in Seattle all of her life?');
+var fromSeattle = prompt('Question 4: Has Susanna lived in Seattle all of her life?');
 
 if (fromSeattle.toLowerCase() === 'yes' || fromSeattle.toLowerCase() === 'y') {
   alert('That\'s incorrect.  She grew up in Eastern Washington, but has lived in Seattle for 9 years.');
 } else {
   alert('That\'s correct! She grew up in Eastern Washington, but has lived in Seattle for 9 years.');
+  totalCorrect++;
 }
 console.log('The answer given to fromSeattle is ' + fromSeattle);
 
 //Prompts asking if Susanna has 3 siblings.
-var siblings = prompt('Does Susanna have 3 siblings?');
+var siblings = prompt('Question 5: Does Susanna have 3 siblings?');
 
 if (siblings.toLowerCase() === 'yes' || siblings.toLowerCase() === 'y') {
   alert('That\'s correct! She has 2 brothers and 1 sister.');
+  totalCorrect++;
 } else {
   alert('That\'s incorrect! She has 2 brothers and 1 sister.');
 }
 console.log('The answer given to siblings is ' + siblings);
 
 //A guessing game to guess how many years I've been married.
-var yearsMarried = prompt(userName + ', how many years have I been married?')
+var yearsMarried = prompt('Question 6: how many years has Susanna been married?')
 console.log('The user entered the number ' + yearsMarried + ' for yearsMarried.');
 for (var i = 0; i < 4; i++) {
   if (yearsMarried > 8) {
@@ -72,37 +80,36 @@ for (var i = 0; i < 4; i++) {
     alert('That\'s correct!')
     break;
   }
-*/
 
-//A game to guess a Seattle neighborhood I have lived in.
+  //A game to guess a Seattle neighborhood I have lived in.
 
-var seattleNeighborhoods = ['lower queen anne', 'capitol hill', 'ballard', 'magnolia'];
-var numGuesses = 0;
-var neighborhoodGuess;
-var correct = false;
+  var seattleNeighborhoods = ['lower queen anne', 'capitol hill', 'ballard', 'magnolia'];
+  var numGuesses = 0;
+  var neighborhoodGuess;
+  var correct = false;
 
-while (numGuesses < 6) {
-  neighborhoodGuess = prompt('Can you guess a Seattle neighborhood that I\'ve lived in?');
-  console.log('The user guessed ' + neighborhoodGuess + ' for neighborhoodGuess.');
-  if (correct === false) {
-    for (var index = 0; index < seattleNeighborhoods.length; index++) {
-      console.log('The number of guessess is ' + numGuesses);
-      console.log('The index is at ' + index);
-      if (neighborhoodGuess.toLowerCase() === seattleNeighborhoods[index]) {
-        console.log('Yay, that\'s right!');
-        alert('Well done, that\'s correct!  I\'ve lived in Lower Queen Anne, Ballard, Capitol Hill, and Magnolia.');
-        console.log('The user guessed ' + neighborhoodGuess + ' for neighborhoodGuess.');
-        correct = true;
-        break;
+  while (numGuesses < 6) {
+    neighborhoodGuess = prompt('Question 7: Can you guess a Seattle neighborhood that Susanna has lived in?');
+    console.log('The user guessed ' + neighborhoodGuess + ' for neighborhoodGuess.');
+    if (correct === false) {
+      for (var index = 0; index < seattleNeighborhoods.length; index++) {
+        console.log('The number of guessess is ' + numGuesses);
+        console.log('The index is at ' + index);
+        if (neighborhoodGuess.toLowerCase() === seattleNeighborhoods[index]) {
+          console.log('Yay, that\'s right!');
+          alert('Well done, that\'s correct!  I\'ve lived in Lower Queen Anne, Ballard, Capitol Hill, and Magnolia.');
+          console.log('The user guessed ' + neighborhoodGuess + ' for neighborhoodGuess.');
+          correct = true;
+          break;
+        }
       }
+    } if (correct === true) {
+      break;
     }
-  } if (correct === true) {
-    break;
+    console.log('That is incorrect');
+    alert('Sorry, that\'s incorrect.  Please try again.');
+    numGuesses++;
   }
-  console.log('That is incorrect');
-  alert('Sorry, that\'s incorrect.  Please try again.')
-  numGuesses++;
-
 }
-
+*
 
