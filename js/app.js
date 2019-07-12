2215aac55ac210ca6ef6a7f3efc92c68f5beb866
Sharina Stubbs <sharinastubbs@gmail.com>
2019-07-11 17:13:12 -0700

    Fixed broken counter and updated README.

--- js/app.js
+++ js/app.js
@@ -1,7 +1,6 @@
 'use strict';
 
 
-
 var userName;
 //Variable to track correct answers.
 var totalCorrect = 0;
@@ -12,16 +11,17 @@ var noAlerts = ['That\'s correct!  Hopefully she\'ll get to go someday!', 'That\
 
 var questions = ['Question 1: Has Susanna ever been to Japan?', 'Question 2: Does Susanna enjoy doing yoga?', 'Question 3: Are radishes Susanna\'s favorite vegetable?', 'Question 4: Has Susanna lived in Seattle all of her life?', 'Question 5: Does Susanna have 3 siblings?'];
 
-var points = [0, 1, 0, 0, 1];
+var yesPoints = [0, 1, 0, 0, 1];
+var noPoints = [1,0, 1, 1, 0];
 
-/*
+
 //Asks for the user's name.
 function askName() {
   userName = prompt('What is your name?');
   alert('Hello ' + userName + '!');
   console.log('The user\'s name is ' + userName + '.');
 }
-*/
+
 
 function yesNoQuestions() {
   for(var index=0; index < yesAlerts.length; index++) {
@@ -29,92 +29,12 @@ function yesNoQuestions() {
     
     if (userAnswer === 'yes' || userAnswer === 'y') {
       alert(yesAlerts[index]);
-      totalCorrect += points[index];
+      totalCorrect += yesPoints[index];
     } else {
       alert(noAlerts[index]);
+      totalCorrect += noPoints[index];
     }
-
   }
-}
-
-yesNoQuestions();
-
-/*
-
-//Prompt asking if Susanna has ever been to Japan.
-function askJapan() {
-  var beenToJapan = prompt('Question 1: Has Susanna ever been to Japan?');
-
-  if (beenToJapan.toLowerCase() === 'yes' || beenToJapan.toLowerCase() === 'y') {
-    alert('That\'s incorrect, but she hopes to someday!');
-  } else {
-    alert('That\'s correct!  Hopefully she\'ll get to go someday!');
-    totalCorrect++;
-    console.log('The total correct is ' + totalCorrect);
-  }
-  console.log('The answer given to beenToJapan is ' + beenToJapan);
-}
-
-
-
-// Prompt asking if Susanna enjoys doing yoga.
-function questionYoga() {
-  var enjoysYoga = prompt('Question 2: Does Susanna enjoy doing yoga?');
-
-  if (enjoysYoga.toLowerCase() === 'yes' || enjoysYoga.toLowerCase() === 'y') {
-    alert('That\'s correct! It helps keep her feel low-stress and bendy!');
-    totalCorrect++;
-    console.log('The total correct is ' + totalCorrect);
-  } else {
-    alert('That\'s incorrect!  Susanna enjoys yoga to keep herself flexible and to decrease her stress levels.');
-  }
-  console.log('The answer given to enjoysYoga is ' + enjoysYoga);
-}
-
-
-
-//Prompt asking if Susanna's favorite vegetable is radishes.
-function questionRadish() {
-  var likesRadishes = prompt('Question 3: Are radishes Susanna\'s favorite vegetable?');
-
-  if (likesRadishes.toLowerCase() === 'yes' || likesRadishes.toLowerCase() === 'y') {
-    alert('That is incorrect.  She finds them to be absolutely repulsive!');
-  } else {
-    alert('That\'s correct!  She finds them to be absolutely repulsive!');
-    totalCorrect++;
-    console.log('The total correct is ' + totalCorrect);
-  }
-  console.log('The answer given to likesRadishes is ' + likesRadishes);
-}
-
-
-//Prompts asking if Susanna has lived in Seattle all of her life.
-function questionSeattle() {
-  var fromSeattle = prompt('Question 4: Has Susanna lived in Seattle all of her life?');
-
-  if (fromSeattle.toLowerCase() === 'yes' || fromSeattle.toLowerCase() === 'y') {
-    alert('That\'s incorrect.  She grew up in Eastern Washington, but has lived in Seattle for 9 years.');
-  } else {
-    alert('That\'s correct! She grew up in Eastern Washington, but has lived in Seattle for 9 years.');
-    totalCorrect++;
-    console.log('The total correct is ' + totalCorrect);
-  }
-  console.log('The answer given to fromSeattle is ' + fromSeattle);
-}
-
-
-//Prompts asking if Susanna has 3 siblings.
-function questionSibling(){
-  var siblings = prompt('Question 5: Does Susanna have 3 siblings?');
-
-  if (siblings.toLowerCase() === 'yes' || siblings.toLowerCase() === 'y') {
-    alert('That\'s correct! She has 2 brothers and 1 sister.');
-    totalCorrect++;
-    console.log('The total correct is ' + totalCorrect);
-  } else {
-    alert('That\'s incorrect! She has 2 brothers and 1 sister.');
-  }
-  console.log('The answer given to siblings is ' + siblings);
 }
 
 
@@ -172,17 +92,11 @@ function questionNeighborhoods() {
 
 
 askName();
-askJapan();
-questionYoga();
-questionRadish();
-questionSeattle();
-questionSibling();
+yesNoQuestions();
 questionMarried();
 questionNeighborhoods();
-
 
 
 //Gives the user their final score.
 alert('Thank you for playing, ' + userName + '! Your final score is ' + totalCorrect + ' out of 7.');
 
-*/
